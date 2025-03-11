2baf2169f9f3c1086c22d136f7a1d5bc03fabe1e
Bintu11 <dasprajnyamit2003@gmail.com>
2025-03-11 21:42:11 +0530

    Update App.jsx
    fix the commit issue

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Hero from "./components/Hero";
 import About from "./components/About";
 import Technologies from "./components/Technologies";
 import Experiences from "./components/Experiences";
-import Projects from "./components/projects";
+import Projects from "./components/Projects";
 import Contact from "./components/contact";
 
 const App = () => {
